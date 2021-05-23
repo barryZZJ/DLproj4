35de394f1ba946012642109c34fcccc10a8a8fbc
Bazinga <850462618@qq.com>
2021-05-23 15:14:26 +0800

    mod dataloader, main to DEBUG mode

--- dataloader.py
+++ dataloader.py
@@ -11,24 +11,30 @@ class DealDataset(Dataset):
         读取数据、初始化数据
     """
 
-    def __init__(self, type, transform=None):
+    def __init__(self, type, transform=None, use_aug=True, DEBUG=False):
         dataset_path = json.load(open("data/dataset.json"))["training"]
         self.transform = transform
-        # divide_point = int(len(dataset_path) * 0.8)
-        divide_point = 1
+
+        divide_point = int(len(dataset_path) * 0.8)
+        if DEBUG:
+            divide_point = 1
+
         if type == "train":
             self.train_path = dataset_path[:divide_point]
         else:
-            # self.train_path = dataset_path[divide_point:]
-            self.train_path = dataset_path[:divide_point]
+            if DEBUG:
+                self.train_path = dataset_path[:divide_point]
+            else:
+                self.train_path = dataset_path[divide_point:]
+        self.use_aug = use_aug
+        self.DEBUG = DEBUG
 
     def __getitem__(self, index):
         img_path = "data/" + self.train_path[index]["image"]
         label_path = "data/" + self.train_path[index]["label"]
-        # img_path = img_path.replace("imagesTr", "imagesTr_Processed").replace(".nii.gz",
-        # "_Processed.nii.gz")
-        # label_path = label_path.replace("labelsTr", "labelsTr_Processed").replace(".nii.gz",
-        # "_Labels_Processed.nii.gz")
+        if self.DEBUG:
+            img_path = img_path.replace("imagesTr", "imagesTr_Processed").replace(".nii.gz", "_Processed.nii.gz")
+            label_path = label_path.replace("labelsTr", "labelsTr_Processed").replace(".nii.gz", "_Labels_Processed.nii.gz")
         img = nib.load(img_path).get_fdata()
         label = nib.load(label_path).get_fdata()
         if self.transform is not None:
@@ -44,9 +50,9 @@ class DealDataset(Dataset):
         return len(self.train_path)
 
 
-def load_data(batch_size):
-    train_dataset = DealDataset("train", transform=transforms.ToTensor())
-    test_dataset = DealDataset("test", transform=transforms.ToTensor())
+def load_data(batch_size=8, use_aug=True, DEBUG=False):
+    train_dataset = DealDataset("train", transform=transforms.ToTensor(), use_aug=use_aug, DEBUG=DEBUG)
+    test_dataset = DealDataset("test", transform=transforms.ToTensor(), use_aug=use_aug, DEBUG=DEBUG)
     # 载入数据集
     train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
     test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
