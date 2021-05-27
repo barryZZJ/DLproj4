62696e1c7e3b146d1e47ee37f218b4c33f6c71e8
Du11JK <825174096@qq.com>
2021-05-27 21:00:53 +0800

    dice gpu

--- dice.py
+++ dice.py
@@ -13,3 +13,17 @@ def dice_coef(output, target):  # output为预测结果 target为真实结果
     return (2. * intersection + smooth) / \
            (output.sum() + target.sum() + smooth)
 
+def dice_coef_gpu(output, target):
+    eps = 1e-5
+    N = target.size(0)
+    output_flat = output.view(N, -1)
+    target_flat = target.view(N, -1)
+    
+    tp = torch.sum(output_flat * target_flat, dim=1)
+    fp = torch.sum(output_flat, dim=1) - tp
+    fn = torch.sum(target_flat, dim=1) - tp
+    loss = (2 * tp + eps) / (2 * tp + fp + fn + eps)
+
+    return loss.sum() / N
+
+
