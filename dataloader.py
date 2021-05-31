dfe28c40a98eeec5d51177c55d076ac9165bbab7
iflys66 <[1782445889@qq.com]>
2021-05-31 20:53:47 +0800

    add read2D_image

--- dataloader.py
+++ dataloader.py
@@ -1,6 +1,6 @@
 import json
+import os
 import random
-
 
 import matplotlib
 import nibabel
@@ -14,6 +14,7 @@ from torchvision import transforms
 from scipy import ndimage
 import SimpleITK as sitk
 
+
 # matplotlib.use('TkAgg')
 
 
@@ -22,12 +23,14 @@ class DealDataset(Dataset):
         读取数据、初始化数据
     """
 
-    def __init__(self, type, transform=None, do_resize=False, use_aug=True, auglist=['Lr', 'Ud'], DEBUG=False):
+    def __init__(self, type, transform=None, do_resize=False, use_aug=True, auglist=['Lr', 'Ud'], DEBUG=False,
+                 read2D_image=True):
         self.do_resize = do_resize
         self.use_aug = use_aug
         self.auglist = auglist
         self.DEBUG = DEBUG
         self.transform = transform
+        self.read2D_image = read2D_image
         self.index_list = list(map(str, [index for index in range(0, 130)]))
         if DEBUG:
             divide_point = 1
@@ -37,27 +40,47 @@ class DealDataset(Dataset):
                  'label': 'data/labelsTr_Cut/liver_num_Labels_Cut.nii.gz'.replace("num", index)}
                 for index in train_index]
         else:
-            divide_point = int(len(self.index_list) * 0.8)
-            random.shuffle(self.index_list)  # in place shuffle
-            if type == "train":
-                train_index = self.index_list[:divide_point]
-                self.train_path = [
-                    {'image': 'data/imagesTr_Cut/liver_{}_Cut.nii.gz'.format(index),
-                     'label': 'data/labelsTr_Cut/liver_{}_Labels_Cut.nii.gz'.format(index)}
-                    for index in train_index]
-                if self.use_aug:
-                    for augmethod in self.auglist:
-                        self.train_path.extend([
-                            {'image': f'data/imagesTr_{augmethod}/liver_{index}_{augmethod}.nii.gz',
-                             'label': f'data/labelsTr_{augmethod}/liver_{index}_Labels_{augmethod}.nii.gz'}
-                            for index in train_index])
-                random.shuffle(self.train_path) # in place shuffle
+            if read2D_image:
+                self.train_image_path = os.listdir('data/imagesTr_2d')
+                self.train_label_path = os.listdir('data/labelsTr_2d')
+                divide_point = int(len(self.train_image_path) * 0.8)
+                if type == "train":
+                    self.train_image_path = self.train_image_path[:divide_point]
+                    self.train_label_path = self.train_label_path[:divide_point]
+                    self.train_path = [{'image': self.train_image_path[index],
+                                        'label': self.train_label_path[index]}
+                                       for index in range(len(self.train_image_path))]
+                    random.shuffle(self.index_list)  # in place shuffle
+                else:
+                    self.train_image_path = self.train_image_path[:divide_point]
+                    self.train_label_path = self.train_label_path[:divide_point]
+                    self.train_path = [{'image': self.train_image_path[index],
+                                        'label': self.train_label_path[index]}
+                                       for index in range(len(self.train_image_path))]
+                    random.shuffle(self.index_list)  # in place shuffle
+
             else:
-                train_index = self.index_list[divide_point:]
-                self.train_path = [
-                    {'image': 'data/imagesTr_Cut/liver_{}_Cut.nii.gz'.format(index),
-                     'label': 'data/labelsTr_Cut/liver_{}_Labels_Cut.nii.gz'.format(index)}
-                    for index in train_index]
+                divide_point = int(len(self.index_list) * 0.8)
+                random.shuffle(self.index_list)  # in place shuffle
+                if type == "train":
+                    train_index = self.index_list[:divide_point]
+                    self.train_path = [
+                        {'image': 'data/imagesTr_Cut/liver_{}_Cut.nii.gz'.format(index),
+                         'label': 'data/labelsTr_Cut/liver_{}_Labels_Cut.nii.gz'.format(index)}
+                        for index in train_index]
+                    if self.use_aug:
+                        for augmethod in self.auglist:
+                            self.train_path.extend([
+                                {'image': f'data/imagesTr_{augmethod}/liver_{index}_{augmethod}.nii.gz',
+                                 'label': f'data/labelsTr_{augmethod}/liver_{index}_Labels_{augmethod}.nii.gz'}
+                                for index in train_index])
+                    random.shuffle(self.train_path)  # in place shuffle
+                else:
+                    train_index = self.index_list[divide_point:]
+                    self.train_path = [
+                        {'image': 'data/imagesTr_Cut/liver_{}_Cut.nii.gz'.format(index),
+                         'label': 'data/labelsTr_Cut/liver_{}_Labels_Cut.nii.gz'.format(index)}
+                        for index in train_index]
 
     def shuffle(self):
         random.shuffle(self.train_path)
@@ -65,19 +88,26 @@ class DealDataset(Dataset):
     def __getitem__(self, index):
         img_path = self.train_path[index]["image"]
         label_path = self.train_path[index]["label"]
-        if self.do_resize:
-            img, label = resize(img_path, label_path)
+
+        if self.read2D_image:
+            img = np.load(img_path)
+            label = np.load(label_path)
         else:
-            img = nib.load(img_path).get_fdata(dtype=np.float32)
-            label = nib.load(label_path).get_fdata(dtype=np.float32)
+            if self.do_resize:
+                img, label = resize(img_path, label_path)
+            else:
+                img = nib.load(img_path).get_fdata(dtype=np.float32)
+                label = nib.load(label_path).get_fdata(dtype=np.float32)
 
         if self.transform is not None:
             img = self.transform(img)  # type: torch.Tensor
             label = self.transform(label)
-            x, y, z = img.shape
-            img = img.reshape(-1, x, y, z)
-            label = label.reshape(-1, x, y, z)
-            # img /= 300
+
+            if not self.read2D_image:
+                x, y, z = img.shape
+                img = img.reshape(-1, x, y, z)
+                label = label.reshape(-1, x, y, z)
+                img /= 300
 
         return img, label  # 1 * 128 * 64 * 64
 
@@ -94,7 +124,8 @@ def resize(img_path, label_path):
     label_array = sitk.GetArrayFromImage(label)
     image_divide = True  # 根据label进行目标切分
     image_show = False  # 展示分割后图像
-    image_zoom = False # 缩放图像
+    image_zoom = False  # 缩放图像
+    image_save = True  # 保存numpy
 
     if image_divide:
         # 找到肝脏区域开始和结束的slice，并各向外扩张
@@ -135,6 +166,12 @@ def resize(img_path, label_path):
         img = nib.load(save_filename)
         OrthoSlicer3D(img.dataobj).show()
     print("Preprocessed shape:", img_array.shape, label_array.shape)
+
+    if image_save:
+        for i in range(len(img_array)):
+            np.save(f'data/imagesTr_Numpy/imagesTr_numpy_{i}.npy', img_array[i])
+            np.save(f'data/labelsTr_Numpy/labelsTr_numpy_{i}.npy', label_array[i])
+            print('完成{:.2f}%'.format(i / len(img_array) * 100))
     return img_array, label_array
 
 
@@ -153,6 +190,8 @@ def load_data(batch_size=8, do_resize=False, use_aug=True, auglist=['Lr', 'Ud'],
 
 
 if __name__ == '__main__':
-    resize('data/imagesTr/liver_3.nii.gz',
-           'data/labelsTr/liver_3.nii.gz')
-    # train_loader, test_loader = load_data()
+    # resize('data/imagesTr/liver_3.nii.gz',
+    #        'data/labelsTr/liver_3.nii.gz')
+    x = np.load(f'data/imagesTr_Numpy/imagesTr_numpy_{0}.npy')
+
+    train_loader, test_loader = load_data()
